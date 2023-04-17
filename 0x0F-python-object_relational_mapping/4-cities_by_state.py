3d066338defc12d892660115357f98266a79d7ed
Ore1232 <oreoluwa59keripe@gmail.com>
2023-04-17 09:14:15 -0700

    python

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
+"""
+lists all cities from the database hbtn_0e_4_usa
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-username, password, database = sys.argv[1:]
 
-with MySQLdb.connect("localhost", username, password, database) as db:
-    db.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    results = db.fetchall()
+def list_cities(username, password, database):
+    with MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8"
+    ) as connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT cities.id, cities.name, states.name
+            FROM cities
+            LEFT JOIN states ON cities.state_id = states.id
+            ORDER BY cities.id ASC"""
+        )
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
 
-for row in results:
-    print(row)
 
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: {} username password database".format(argv[0]))
+        exit(1)
+    list_cities(argv[1], argv[2], argv[3])
