fb05b21e802ec92e70dd53a856700167f504ce1e
Ore1232 <oreoluwa59keripe@gmail.com>
2023-04-17 09:03:40 -0700

    python

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""
+Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    conn = MySQLdb.connect('localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC', (sys.argv[4],))
-    for row in cur.fetchall():
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC", (sys.argv[4],))
+
+    for row in cursor.fetchall():
         print(row)
+
+    cursor.close()
+    db.close()
 
