dfb130f5d69102de259077cf7d4b2ee15f1bf675
Ore1232 <oreoluwa59keripe@gmail.com>
2023-04-28 13:08:29 -0700

    python

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+
+def fetch_status():
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    fetch_status()
+
